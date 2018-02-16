43d77d5d9a2ec5b3b0e8487eb465ad6d044b00b5
lanusseq <qlanusse@gmail.com>
2018-02-16 12:43:29 +0100

    Read data init

--- src/Tools/ExcelTools.java
+++ src/Tools/ExcelTools.java
@@ -8,32 +8,69 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 
+import models.Location;
+
 public class ExcelTools {
     public static void readExcelFile(File file) {
         try {
             FileInputStream fs = new FileInputStream(file);
             XSSFWorkbook wb = new XSSFWorkbook(fs);
-            XSSFSheet sheet = wb.getSheetAt(0);
             XSSFRow row;
             XSSFCell cell;
-
-            int rows; // No of rows
-            rows = sheet.getPhysicalNumberOfRows();
 
             int nbSheets;
             nbSheets = wb.getNumberOfSheets();
 
-            int cols = 0; // No of columns
-            int tmp;
+            for(int j = 0 ; j < nbSheets ; j++) {
 
-            // This trick ensures that we get the data properly even if it doesn't start from first few rows
-            for (int i = 0; i < 10 || i < rows; i++) {
-                row = sheet.getRow(i);
-                if (row != null) {
-                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-                    if (tmp > cols) cols = tmp;
+                XSSFSheet sheet = wb.getSheetAt(j);
+
+                int rows; // No of rows
+                rows = sheet.getPhysicalNumberOfRows();
+
+                int cols = 0; // No of columns
+                int tmp;
+
+                // This trick ensures that we get the data properly even if it doesn't start from first few rows
+                for (int i = 0; i < 10 || i < rows; i++) {
+                    row = sheet.getRow(i);
+                    if (row != null) {
+                        tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+                        if (tmp > cols) cols = tmp;
+                    }
+                }
+
+                for (int r = 0; r < rows; r++) {
+                    row = sheet.getRow(r);
+                    if (row != null) {
+                        for (int c = 0; c < cols; c++) {
+                            cell = row.getCell((short) c);
+                            if (cell != null) {
+                                System.out.println(cell.getRawValue());
+                            }
+                        }
+                    }
                 }
             }
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void readProducers(File file){
+        try {
+            FileInputStream fs = new FileInputStream(file);
+            XSSFWorkbook wb = new XSSFWorkbook(fs);
+            XSSFRow row;
+            XSSFCell cell;
+
+            XSSFSheet sheet = wb.getSheet("DonnéesProd");
+
+            int rows; // No of rows
+            rows = sheet.getPhysicalNumberOfRows();
+
+            int cols = 0; // No of columns
+            int tmp;
 
             for (int r = 0; r < rows; r++) {
                 row = sheet.getRow(r);
@@ -46,8 +83,16 @@ public class ExcelTools {
                     }
                 }
             }
-        } catch (Exception ioe) {
+        } catch (Exception ioe){
             ioe.printStackTrace();
         }
+    }
+
+    public static void readCustomers(){
+
+    }
+
+    public static void readHubs(){
+        
     }
 }
